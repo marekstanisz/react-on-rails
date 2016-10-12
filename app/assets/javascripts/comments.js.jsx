1667e046c96ad151c1e4191e374c2a5a1857d3a5
Marek Stanisz <m.stanisz@binarapps.com>
2016-10-12 12:35:31 +0200

    remove unnecessary console.log

--- app/assets/javascripts/comments.js.jsx
+++ app/assets/javascripts/comments.js.jsx
@@ -85,7 +85,6 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
-    console.log(this.props.data);
     var commentNodes = this.props.data.map(function(comment) {
       return (
         <Comment author={comment.author} key={comment.id}>
